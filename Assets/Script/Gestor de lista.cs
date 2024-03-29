835a48a153e792aa54d9c80dfc140d6a016b4a58
LordRenaldo <152116142+LordRenaldo@users.noreply.github.com>
2024-03-29 00:29:25 -0300

    Modificaciones en la UI

--- Assets/Script/Gestor de lista.cs
+++ Assets/Script/Gestor de lista.cs
@@ -8,7 +8,7 @@ public class Gestordelista : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log ("star");
     }
 
     // Update is called once per frame
