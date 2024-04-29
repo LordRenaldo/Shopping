670b325dba377efcea3bbbcd6112eafbc3cf6ed3
LordRenaldo <152116142+LordRenaldo@users.noreply.github.com>
2024-04-29 02:35:49 -0300

    Update Gestor de botones.cs

--- Assets/Script/Gestor de botones.cs
+++ Assets/Script/Gestor de botones.cs
@@ -3,15 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class Gestordebotones : MonoBehaviour
 {
-    Gestordelista Manager;
     public void ChangeOfScene ( int scenaP )
     {
         SceneManager.LoadScene (scenaP);
     }
 
-    public void ChargeListFinal ()
-    {
-        SceneManager.LoadScene (2);
-        Manager.DisplayFinalListItems ();
-    }
 }
