96c11c694076931c7e8a4cd6aa64275147925214
LordRenaldo <152116142+LordRenaldo@users.noreply.github.com>
2024-05-09 02:29:10 -0300

    Optimizado
    Se cambio de lugar algunos métodos.
    Se coloco un botón para cerrar la app

--- Assets/Script/Gestor de botones.cs
+++ Assets/Script/Gestor de botones.cs
@@ -1,43 +1,93 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Gestordebotones : MonoBehaviour
 {
+    [SerializeField]
+    Button prefabBotton;
+    [SerializeField]
+    GameObject content;
+    Vector3 ultimaPosicion;
+    [SerializeField]
+    float diferencia = 400f;
+    private Gestordelista gestordelista;
+    private List<string> todosLosProductos;
+    private void Awake ()
+    {
+        gestordelista = FindObjectOfType<Gestordelista> ();
+    }
+
+    public void Start ()
+    {
+        ultimaPosicion = Vector3.zero;
+        todosLosProductos = gestordelista.CreateListOfAllArticles (gestordelista.CreateAListOfNonPerishable (), gestordelista.CreateListOfFruitsAndVegetables ());
+
+        foreach (var articulos in todosLosProductos)
+        {
+            InstantiateButton (articulos);
+        }
+    }
     public void ChangeOfScene ( int scenaP )
     {
         SceneManager.LoadScene (scenaP);
     }
-
-    public void ReceiveAndPrintList ( List<string> listaRecibida )
+    public void Retrea ()//aqui genio, el problema es aqui!!!
     {
-        foreach (string elemento in listaRecibida)
+        SceneManager.LoadScene (1);
+        todosLosProductos = gestordelista.CreateListOfAllArticles (gestordelista.CreateAListOfNonPerishable (), gestordelista.CreateListOfFruitsAndVegetables ());
+        foreach (var articulos in todosLosProductos)
+        {
+            Debug.Log ("entro el el ciclo de retrea");
+            InstantiateButton (articulos);
+        }
+    }
+    public void ReceiveAndPrintList ( List<string> listaRecibidaP )
+    {
+        foreach (string elemento in listaRecibidaP)
         {
             Debug.Log (elemento);
         }
 
-        GameObject [] botones = GameObject.FindGameObjectsWithTag ("Button list");
-
-        foreach (GameObject boton in botones)
+        var botones = GameObject.FindGameObjectsWithTag ("Button list");
+        foreach (var boton in botones)
         {
-            Debug.Log (boton + " destruido");
             Destroy (boton);
         }
-        Gestordelista gestordelista = FindObjectOfType<Gestordelista> ();
-        gestordelista.DisplayFinalListItems ();
 
+        gestordelista.DisplayFinalListItems ();
+        var BotonCrearLista = GameObject.Find ("Button crear lista");
+        var textNewBoton = BotonCrearLista.GetComponentInChildren<TextMeshProUGUI> ();
+        textNewBoton.text = "Volver a la lista";
+        BotonCrearLista.GetComponent<Button> ().onClick.AddListener (() => { ChangeOfScene (1); });
+
+    }
+    private void InstantiateButton ( string nombreArticuloP )
+    {
+        Debug.Log ("entro en el ciclo de instanciar botones");
+        var newButton = Instantiate (prefabBotton, content.transform);
+        newButton.onClick.AddListener (() =>
+        {
+            gestordelista.addToNewList (nombreArticuloP);
+            Destroy (newButton.gameObject);
+        });
+
+        var textNewBoton = newButton.GetComponentInChildren<TextMeshProUGUI> ();
+        textNewBoton.text = nombreArticuloP;
+
+        newButton.transform.localPosition = ultimaPosicion;
+        ultimaPosicion -= new Vector3 (0f, diferencia, 0f);
     }
 
     public void ExitApp ()
     {
-        if (Application.isEditor)
-        {
-            UnityEditor.EditorApplication.isPlaying = false;
-        }
-        else
-        {
-            Application.Quit ();
-        }
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
         Debug.Log ("Aplicación cerrada");
     }
 }
--- Assets/Script/Gestor de lista.cs
+++ Assets/Script/Gestor de lista.cs
@@ -1,272 +1,115 @@
 using System.Collections.Generic;
-using System.Linq;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Gestordelista : MonoBehaviour
 {
     [SerializeField]
     TextMeshProUGUI textPrefab;
     [SerializeField]
-    Button prefabBotton;
+    float diferencia = 400f;
     [SerializeField]
     GameObject content;
+    [HideInInspector]
     Vector3 ultimaPosicion;
-    [SerializeField]
-    float diferencia = 400f;
-
     [HideInInspector]
     public List<string> listaFinal = new List<string> ();
 
-    public TextMeshProUGUI TextPrefab { get => textPrefab; set => textPrefab = value; }
-    public Button PrefabBotton { get => prefabBotton; set => prefabBotton = value; }
-    public GameObject Content { get => content; set => content = value; }
-    public Vector3 UltimaPosicion { get => ultimaPosicion; set => ultimaPosicion = value; }
-    public float Diferencia { get => diferencia; set => diferencia = value; }
-    public List<string> ListaFinal { get => listaFinal; set => listaFinal = value; }
-
-
-    // Start is called before the first frame update
     void Start ()
     {
-        ultimaPosicion = Vector3.zero;
-        List<string> listaFrutas = CreateListOfFruitsAndVegetables ();
-        List<string> listaMercado = CreateAListOfNonPerishable ();
-        List<string> todosLosProductos = CreateListOfAllArticles (listaMercado, listaFrutas);
-
-        foreach (string articulos in todosLosProductos)
-        {
-            InstantiateButton (articulos);
-        }
 
     }
+
     void Awake ()
     {
 
-        DontDestroyOnLoad (this.gameObject);
-
     }
 
-    // Update is called once per frame
-    void Update ()
+    public List<string> CreateListOfAllArticles ( List<string> list1P, List<string> list2P )
     {
-
-    }
-    private List<string> CreateListOfAllArticles ( List<string> list1P, List<string> list2P )
-    {
-        List<string> newlist = new List<string> ();
-
-        newlist.AddRange (list1P);
+        var newlist = new List<string> (list1P);
         newlist.AddRange (list2P);
-
-        List<string> ListaTotal = SortAlphabetically (newlist);
-        ListaTotal.Reverse ();
-        return ListaTotal;
+        newlist.Sort ();
+        newlist.Reverse ();
+        return newlist;
     }
-    private List<string> CreateAListOfNonPerishable ()
+
+    public List<string> CreateAListOfNonPerishable ()
     {
-        List<string> articles = new List<string> ();
-
-        articles.Add ("Arroz");
-        articles.Add ("Fideos");
-        articles.Add ("Laminas de pasticho");
-        articles.Add ("Azucar");
-        articles.Add ("Sal");
-        articles.Add ("Leche liquida");
-        articles.Add ("Leche en polvo");
-        articles.Add ("Harina pan");
-        articles.Add ("Harina maiz");
-        articles.Add ("Harina pizza");
-        articles.Add ("Harina leudante");
-        articles.Add ("Pure de tomate");
-        articles.Add ("Yogur Liquido");
-        articles.Add ("Yogur firme");
-        articles.Add ("Salchicha");
-        articles.Add ("Pan de pancho");
-        articles.Add ("Mostaza");
-        articles.Add ("Mayonesa");
-        articles.Add ("Ketchup");
-        articles.Add ("Manteca");
-        articles.Add ("Margarina");
-        articles.Add ("Grasa vegetal");
-        articles.Add ("Poroto");
-        articles.Add ("Pochoclo");
-
-        articles = SortAlphabetically (articles);
-
+        var articles = new List<string>
+            {
+                "Arroz", "Fideos", "Laminas de pasticho", "Azucar", "Sal", "Leche liquida", "Leche en polvo", "Harina pan",
+                "Harina maiz", "Harina pizza", "Harina leudante", "Pure de tomate", "Yogur Liquido", "Yogur firme", "Salchicha",
+                "Pan de pancho", "Mostaza", "Mayonesa", "Ketchup", "Manteca", "Margarina", "Grasa vegetal", "Poroto", "Pochoclo"
+            };
+        articles.Sort ();
         return articles;
     }
-    private List<string> CreateListOfFruitsAndVegetables ()
+
+    public List<string> CreateListOfFruitsAndVegetables ()
     {
-        List<string> fruitsAndVegetables = new List<string> ();
-
-        fruitsAndVegetables.Add ("Acelga");
-        fruitsAndVegetables.Add ("Aji amarillo");
-        fruitsAndVegetables.Add ("Aji jalapeño");
-        fruitsAndVegetables.Add ("Aji picante");
-        fruitsAndVegetables.Add ("Aji rocoto");
-        fruitsAndVegetables.Add ("Aji rojo");
-        fruitsAndVegetables.Add ("Ajo");
-        fruitsAndVegetables.Add ("Albahaca");
-        fruitsAndVegetables.Add ("Alfalfa");
-        fruitsAndVegetables.Add ("Anana");
-        fruitsAndVegetables.Add ("Arandanos");
-        fruitsAndVegetables.Add ("Banana");
-        fruitsAndVegetables.Add ("Berenjena");
-        fruitsAndVegetables.Add ("Berros");
-        fruitsAndVegetables.Add ("Brocoli");
-        fruitsAndVegetables.Add ("Bruselas");
-        fruitsAndVegetables.Add ("Camote");
-        fruitsAndVegetables.Add ("Cebolla blanca");
-        fruitsAndVegetables.Add ("Cebolla escabeche");
-        fruitsAndVegetables.Add ("Cebolla morada");
-        fruitsAndVegetables.Add ("Cereza");
-        fruitsAndVegetables.Add ("Chirimoya");
-        fruitsAndVegetables.Add ("Choclo");
-        fruitsAndVegetables.Add ("Cilantro");
-        fruitsAndVegetables.Add ("Ciruela");
-        fruitsAndVegetables.Add ("Coco");
-        fruitsAndVegetables.Add ("Huevo codorniz");
-        fruitsAndVegetables.Add ("Coliflor");
-        fruitsAndVegetables.Add ("Zapallo Coreano");
-        fruitsAndVegetables.Add ("Zapallo italiano");
-        fruitsAndVegetables.Add ("Zapallo redondo");
-        fruitsAndVegetables.Add ("Nispero");
-        fruitsAndVegetables.Add ("Sandia");
-        fruitsAndVegetables.Add ("Damasco");
-        fruitsAndVegetables.Add ("Durazno blanco");
-        fruitsAndVegetables.Add ("Durazno amarillo");
-        fruitsAndVegetables.Add ("Durazno chato");
-        fruitsAndVegetables.Add ("Durazno pelon");
-        fruitsAndVegetables.Add ("Durazno prisco");
-        fruitsAndVegetables.Add ("Echalote");
-        fruitsAndVegetables.Add ("Escarola");
-        fruitsAndVegetables.Add ("Esparragos");
-        fruitsAndVegetables.Add ("Espinaca");
-        fruitsAndVegetables.Add ("Berenjena BB");
-        fruitsAndVegetables.Add ("Espinaca BB");
-        fruitsAndVegetables.Add ("Franbuesa");
-        fruitsAndVegetables.Add ("Frutilla");
-        fruitsAndVegetables.Add ("Jengibre");
-        fruitsAndVegetables.Add ("Habas");
-        fruitsAndVegetables.Add ("Kiwi");
-        fruitsAndVegetables.Add ("Laurel");
-        fruitsAndVegetables.Add ("Lechuga mante");
-        fruitsAndVegetables.Add ("Lechuga morada");
-        fruitsAndVegetables.Add ("Lechuga repollada");
-        fruitsAndVegetables.Add ("Lechuga romana");
-        fruitsAndVegetables.Add ("Lechuga rulito");
-        fruitsAndVegetables.Add ("Lima");
-        fruitsAndVegetables.Add ("Limon");
-        fruitsAndVegetables.Add ("Limon regilla");
-        fruitsAndVegetables.Add ("Mandarina");
-        fruitsAndVegetables.Add ("Mandioca");
-        fruitsAndVegetables.Add ("Mango");
-        fruitsAndVegetables.Add ("Manzana roja");
-        fruitsAndVegetables.Add ("Manzana verde");
-        fruitsAndVegetables.Add ("Melon");
-        fruitsAndVegetables.Add ("Menbrillo");
-        fruitsAndVegetables.Add ("Moras");
-        fruitsAndVegetables.Add ("Nabos");
-        fruitsAndVegetables.Add ("Naranja jugo");
-        fruitsAndVegetables.Add ("Naranja");
-        fruitsAndVegetables.Add ("Palta");
-        fruitsAndVegetables.Add ("Papa");
-        fruitsAndVegetables.Add ("Papa lavada");
-        fruitsAndVegetables.Add ("Papa resto");
-        fruitsAndVegetables.Add ("Papa roja");
-        fruitsAndVegetables.Add ("Papaya");
-        fruitsAndVegetables.Add ("Papin");
-        fruitsAndVegetables.Add ("Pepinillo");
-        fruitsAndVegetables.Add ("Oregano");
-        fruitsAndVegetables.Add ("Pepino");
-        fruitsAndVegetables.Add ("Pera");
-        fruitsAndVegetables.Add ("Perejil");
-        fruitsAndVegetables.Add ("Pimiento amarillo");
-        fruitsAndVegetables.Add ("Pimiento Rojo");
-        fruitsAndVegetables.Add ("Pimiento verde");
-        fruitsAndVegetables.Add ("Platano");
-        fruitsAndVegetables.Add ("Pomelo");
-        fruitsAndVegetables.Add ("Puerro");
-        fruitsAndVegetables.Add ("Rabanito");
-        fruitsAndVegetables.Add ("Remolacha");
-        fruitsAndVegetables.Add ("Repollo blanco");
-        fruitsAndVegetables.Add ("Repollo morado");
-        fruitsAndVegetables.Add ("Romero");
-        fruitsAndVegetables.Add ("Rucula");
-        fruitsAndVegetables.Add ("Tomate perita");
-        fruitsAndVegetables.Add ("Tomate");
-        fruitsAndVegetables.Add ("Tomillo");
-        fruitsAndVegetables.Add ("Menta");
-        fruitsAndVegetables.Add ("Uva red globe");
-        fruitsAndVegetables.Add ("Uva moscatela");
-        fruitsAndVegetables.Add ("Uva blanca");
-        fruitsAndVegetables.Add ("Apio españa");
-        fruitsAndVegetables.Add ("Verdeo");
-        fruitsAndVegetables.Add ("Zanahoria");
-        fruitsAndVegetables.Add ("Zanahoria bb");
-        fruitsAndVegetables.Add ("Zanahoria ");
-        fruitsAndVegetables.Add ("Leña");
-
-        fruitsAndVegetables = SortAlphabetically (fruitsAndVegetables);
-
+        var fruitsAndVegetables = new List<string>
+            {
+                "Acelga", "Aji amarillo", "Aji jalapeño", "Aji picante", "Aji rocoto", "Aji rojo", "Ajo", "Albahaca", "Alfalfa",
+                "Anana", "Arandanos", "Banana", "Berenjena", "Berros", "Brocoli", "Bruselas", "Camote", "Cebolla blanca",
+                "Cebolla escabeche", "Cebolla morada", "Cereza", "Chirimoya", "Choclo", "Cilantro", "Ciruela", "Coco",
+                "Huevo codorniz", "Coliflor", "Zapallo Coreano", "Zapallo italiano", "Zapallo redondo", "Nispero", "Sandia",
+                "Damasco", "Durazno blanco", "Durazno amarillo", "Durazno chato", "Durazno pelon", "Durazno prisco", "Echalote",
+                "Escarola", "Esparragos", "Espinaca", "Berenjena BB", "Espinaca BB", "Franbuesa", "Frutilla", "Jengibre", "Habas",
+                "Kiwi", "Laurel", "Lechuga mante", "Lechuga morada", "Lechuga repollada", "Lechuga romana", "Lechuga rulito",
+                "Lima", "Limon", "Limon regilla", "Mandarina", "Mandioca", "Mango", "Manzana roja", "Manzana verde", "Melon",
+                "Menbrillo", "Moras", "Nabos", "Naranja jugo", "Naranja", "Palta", "Papa", "Papa lavada", "Papa resto", "Papa roja",
+                "Papaya", "Papin", "Pepinillo", "Oregano", "Pepino", "Pera", "Perejil", "Pimiento amarillo", "Pimiento Rojo",
+                "Pimiento verde", "Platano", "Pomelo", "Puerro", "Rabanito", "Remolacha", "Repollo blanco", "Repollo morado",
+                "Romero", "Rucula", "Tomate perita", "Tomate", "Tomillo", "Menta", "Uva red globe", "Uva moscatela", "Uva blanca",
+                "Apio españa", "Verdeo", "Zanahoria", "Zanahoria bb", "Zanahoria ", "Leña"
+            };
+        fruitsAndVegetables.Sort ();
         return fruitsAndVegetables;
     }
-    private List<string> SortAlphabetically ( List<string> listP )
-    {
-        return listP.OrderBy (x => x).ToList ();
-    }
-    private void InstantiateButton ( string nombreArticulo )
-    {
-        GameObject manager = GameObject.Find ("Manager");
-        Gestordelista script = manager.GetComponentInChildren<Gestordelista> ();
-        Button newButton = Instantiate (PrefabBotton, Content.transform);
-        newButton.onClick.AddListener (() =>
-        {
-            addToNewList (nombreArticulo);
-            Destroy (newButton.gameObject);
-        });
 
-        TextMeshProUGUI textNewBoton = newButton.GetComponentInChildren<TextMeshProUGUI> ();
-        textNewBoton.text = nombreArticulo;
-
-        newButton.transform.localPosition = UltimaPosicion;
-        UltimaPosicion -= new Vector3 (0f, Diferencia, 0f);
-    }
-    public void addToNewList ( string Articulo )
+    public void addToNewList ( string ArticuloP )
     {
-        ListaFinal.Add (Articulo);
-        Debug.Log (Articulo + " Agregado a la lista de compras");
+        listaFinal.Add (ArticuloP);
+        Debug.Log (ArticuloP + " Agregado a la lista de compras");
     }
+
     public void PrintFinalListItems ()
     {
         Debug.Log ("Lista de compras final");
-        foreach (string item in ListaFinal)
+        foreach (var item in listaFinal)
         {
             Debug.Log (item);
         }
     }
+
     public void DisplayFinalListItems ()
     {
-        PrintFinalListItems ();
-
-        foreach (string item in ListaFinal)
+        if (content == null)
         {
-            TextMeshProUGUI newText = Instantiate (TextPrefab, Content.transform);
-            newText.transform.localPosition = UltimaPosicion;
-            UltimaPosicion -= new Vector3 (0f, Diferencia, 0f);
-            newText.text = item;
-            Debug.Log ("DisplayFinalListItems llamado");
+            Debug.LogError ("El objeto 'content' no existe.");
+            return;
         }
 
+        foreach (var item in listaFinal)
+        {
+            if (textPrefab == null)
+            {
+                Debug.LogError ("textPrefab no está asignado.");
+                return;
+            }
+
+            var newText = Instantiate (textPrefab, content.transform);
+            newText.transform.localPosition = ultimaPosicion;
+            ultimaPosicion -= new Vector3 (0f, diferencia, 0f);
+            newText.text = item;
+        }
     }
+
     public void SendList ()
     {
-        Gestordebotones gestordebotones = FindObjectOfType<Gestordebotones> ();
-
+        var gestordebotones = FindObjectOfType<Gestordebotones> ();
         gestordebotones.ReceiveAndPrintList (listaFinal);
-
     }
 }
 
